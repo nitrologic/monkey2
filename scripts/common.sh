9a9cc6776ec5b47c65b516ef8f341aea2d7645b3
Simon Armstrong <nitrologic@gmail.com>
2016-07-18 14:21:32 +1200

    pi scripts suport

--- scripts/common.sh
+++ scripts/common.sh
@@ -1,4 +1,7 @@
 
+# stop on first error
+set -e
+
 mx2cc=""
 mx2cc_new=""
 ted2=""
@@ -10,22 +13,38 @@ if [ "$OSTYPE" = "linux-gnu" ]
 then
 
 	mx2cc="../bin/mx2cc_linux"
-	mx2cc_new="../src/mx2cc/mx2cc.buildv1.0.2/desktop_release_linux/mx2cc"
+	mx2cc_new="../src/mx2cc/mx2cc.buildv1.0.0/desktop_release_linux/mx2cc"
 	
 	ted2="../bin/ted2_linux"
-	ted2_new="../src/ted2/ted2.buildv1.0.2/desktop_release_linux"
+	ted2_new="../src/ted2/ted2.buildv1.0.0/desktop_release_linux"
 	
 	launcher="../Monkey2 (Linux)"
-	launcher_new="../src/launcher/launcher.buildv1.0.2/desktop_release_linux/launcher"
+	launcher_new="../src/launcher/launcher.buildv1.0.0/desktop_release_linux/launcher"
 	
-else
+fi
+
+if [ "$OSTYPE" = "linux-gnueabihf" ]
+then
+	mx2cc="../bin/mx2cc_pi"
+	mx2cc_new="../src/mx2cc/mx2cc.buildv1.0.0/desktop_release_pi/mx2cc"
+	
+	ted2="../bin/ted2_pi"
+	ted2_new="../src/ted2/ted2.buildv1.0.0/desktop_release_pi"
+	
+	launcher="../Monkey2 (Pi)"
+	launcher_new="../src/launcher/launcher.buildv1.0.0/desktop_release_pi/launcher"
+	
+fi
+
+if [ "$OSTYPE" = "darwin15" ]
+then
 
 	mx2cc="../bin/mx2cc_macos"
-	mx2cc_new="../src/mx2cc/mx2cc.buildv1.0.2/desktop_release_macos/mx2cc"
+	mx2cc_new="../src/mx2cc/mx2cc.buildv1.0.0/desktop_release_macos/mx2cc"
 	
 	ted2="../bin/ted2_macos.app"
-	ted2_new="../src/ted2/ted2.buildv1.0.2/desktop_release_macos/ted2.app"
+	ted2_new="../src/ted2/ted2.buildv1.0.0/desktop_release_macos/ted2.app"
 	
 	launcher="../Monkey2 (Macos).app"
-	launcher_new="../src/launcher/launcher.buildv1.0.2/desktop_release_macos/launcher.app"
+	launcher_new="../src/launcher/launcher.buildv1.0.0/desktop_release_macos/launcher.app"
 fi
